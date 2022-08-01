2b07a17e01f6c91a955878731a17ed7837f5428f
Amilcov <Amilcov@yahoo.com>
2022-08-02 01:16:50 +0300

    Add files via upload

---
+++ __CV/Surse/3. Amusement Park/config/database.js
@@ -0,0 +1,29 @@
+const {
+  username,
+  password,
+  database,
+  host,
+} = require('./index').db;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+  },
+
+production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION 
+      },
+    },
+  },
+
+
+};
