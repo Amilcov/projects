4dec7be9fad20803a058c9fdd90203ee0ee8989f
Amilcov <Amilcov@yahoo.com>
2022-08-01 12:01:42 +0300

    Add files via upload

---
+++ __CV/Surse/1. Mini Twitter/Client/express-apis-frontend/public/js/log-in.js
@@ -0,0 +1,60 @@
+const loginForm = document.querySelector('.log-in-form')
+
+loginForm.addEventListener('submit' , async (e) => {
+    e.preventDefault();
+    const formData = new FormData(loginForm);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const body = { email, password };
+
+   
+    try {
+        const res = await fetch("https://am-mini-twitter-server.herokuapp.com//users/token", {
+        //const res = await fetch("http://localhost:8080/users/token", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+ 
+        if (!res.ok) {
+            throw res;
+        };
+
+        const { token, user: {id} } = await res.json();
+      
+        localStorage.setItem('TWITTER_LITE_ACCESS_TOKEN', token);
+        localStorage.setItem('TWITTER_LITE_CURRENT_USER_ID', id);
+        
+        window.location.href = '/';
+
+    } catch(err) {
+        
+        if (err.status >= 400 && err.status < 600) {
+            
+            const errorsJSON = await err.json();
+            let errorsContainer = document.querySelector('.errors-container');
+
+            let errorsHTML = [`
+              <div class="alert alert-danger>
+                  Something went wrong. Please try again.
+              </div>
+           `];
+
+            const { errors } = errorsJSON;
+            if (errors && Array.isArray(errors)) {
+                errorsHTML = errors.map( message => `
+                    <div class='alert alert-danger'>
+                      ${message}
+                    </div>
+                `)
+            };
+           
+            errorsContainer.innerHTML = errorsHTML.join("");
+        } else {
+            alert('Something went wrong. Check internet connection and try again!');
+        }; 
+    }
+})
---
+++ __CV/Surse/1. Mini Twitter/Client/express-apis-frontend/public/js/profile.js
@@ -0,0 +1,147 @@
+const token = localStorage.getItem('TWITTER_LITE_ACCESS_TOKEN');
+
+document.addEventListener('DOMContentLoaded', async (e) => {
+    e.preventDefault();
+
+    try {
+        const userId = localStorage.getItem('TWITTER_LITE_CURRENT_USER_ID');
+
+        const res = await fetch(`https://am-mini-twitter-server.herokuapp.com/users/${userId}/tweets`, {
+        //const res = await fetch(`http://localhost:8080/users/${userId}/tweets`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+   
+
+        if (res.status === 401) {
+            window.location.href = '/log-in';
+            return;
+        };
+
+
+
+        if (!res.ok) {
+            throw res;
+        }
+ 
+        const tweets = await res.json();
+        const tweetsContainer = document.querySelector('.tweets-container');
+        
+        const tweetsHTLM = tweets.map( ({message, id}) =>  `
+          <div class="card" id="tweet-${id}">
+            <div class="card-body">
+              <p class="card-text"> ${message} </p>
+              <button class="btn btn-danger delete-button" id="${id}"> Delete </button>
+             </div>
+        
+          </div>
+        ` 
+        );
+        tweetsContainer.innerHTML = tweetsHTLM.join("");
+
+    } catch(err) {
+
+        if(err.status >= 400 && err.status < 600) {
+           console.error(err);
+        } else {
+           alert('Something went wrong. Please check internet connection and try again');
+        }
+
+    };
+
+
+    const deleteButtons = document.querySelectorAll('.delete-button');
+    if (deleteButtons) {
+        deleteButtons.forEach( (button) => {
+            button.addEventListener('click', handleDelete);
+        });
+    
+    };
+    
+});
+
+
+    
+
+  
+
+
+
+async function handleDelete(id) {
+ console.log(` FC HANDLE Delete this.id= ${this.id}, id= ${id}`);
+
+    try {
+    
+        const res = await fetch(`http://localhost:8080/tweets/${this.id}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        console.log('here1');
+
+        if (res.status === 401) {
+           window.location.href = '/log-in';
+        };
+
+         console.log('here2');
+
+        if (!res.ok) {
+           throw res;
+        };
+
+         console.log('here3');
+        window.location.href = '/';
+
+
+    } catch(err) {
+         
+       if (err.status >= 400 && err.status < 600 ) {
+
+          const errorsJSON = await res.json();
+          const errorsContainer = document.querySelector('.errors-container'); 
+
+          let errorsHTML = [`
+             <div class="card card-danger">
+                <p>  Something went wrong. Please try again.</p>
+             </div>
+          `];
+
+           const { errors } = errorsJSON;
+           if ( errors && Array.isArray(errors)) {
+              errorsHTML = errors.map( (message) => {
+               ` 
+                 <div class="card card-danger"> 
+                    <p> ${message}</p>
+                  </div>
+               `
+           });
+        }
+
+       errorsContainer.innerHTML = errorsHTML.join("");
+
+
+       } else {
+         console.error('Something went wrong. Please ceck internet connection and try again.')
+       }
+
+        // const result = await res.json();
+        // if (err.status >= 401 && err.status < 600) {
+
+        // } else {
+        //     console.error('Something went wrong. Please check internet connection and try again.')
+        // }
+    }
+
+    
+ 
+  
+      
+  
+}
+
