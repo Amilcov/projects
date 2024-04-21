e71eee6e6f1639bfd3d8ed69401f881838e8991f
Amilcov <Amilcov@yahoo.com>
2024-04-21 15:35:15 +0300

    Add files via upload

--- __CV/Surse/0.CV/script.js
+++ __CV/Surse/0.CV/script.js
@@ -1,59 +1,59 @@
 window.onload = () => {
-    const h1 = document.createElement('h1');
-    h1.setAttribute('id', 'name');
-    const name = document.createTextNode('Adriana Claudia Milcov');
-    h1.appendChild(name);
-    document.body.appendChild(h1);
+  const h1 = document.createElement('h1');
+  h1.setAttribute('id', 'name');
+  const name = document.createTextNode('Adriana Claudia Milcov');
+  h1.appendChild(name);
+  document.body.appendChild(h1);
 
-    const picture = document.createElement('div');
-    picture.setAttribute("class", "picture");
-    picture.setAttribute("id", "pictureId");
-    document.body.appendChild(picture);
-
-
-    function daysUntilMyBDay() {
-        const today = new Date();
-        let myNextBDay = new Date(today.getFullYear(), 09, 02);
-        let daysBetween = '';
-      
-
-        if (today.getMonth() === 9 && today.getDate === 2) {
-            daysBetween =  'Happy Birth Day!!'
-        } else {
-             console.log('month', today.getMonth() );
-              console.log('day', today.getDay());
-              /*
-            if (today.getMonth() > 9 || (today.getMonth() === 9 &&  today.getDate() > 2) ){
-              let mynextYearBday = today.getFullYear() + 1;
-               myNextBDay = new Date(mynextYearBday, 09, 02);
-                 console.log('myNextBDay', myNextBDay );
-            }
-            */
-           daysBetween = parseInt((myNextBDay.getTime()- today.getTime())/ (24* 3600 * 1000)) ;
-           daysBetween= daysBetween < 0 ? Math.abs(daysBetween) + ' days ago': daysBetween + 'days';
-        }     
+  const picture = document.createElement('div');
+  picture.setAttribute("class", "picture");
+  picture.setAttribute("id", "pictureId");
+  document.body.appendChild(picture);
 
 
-        return daysBetween;
+  function daysUntilMyBDay() {
+    const today = new Date();
+    let myNextBDay = new Date(today.getFullYear(), 09, 02);
+    let daysBetween = '';
+
+
+    if (today.getMonth() === 9 && today.getDate === 2) {
+      daysBetween = 'Happy Birth Day!!'
+    } else {
+      console.log('month', today.getMonth());
+      console.log('day', today.getDay());
+      /*
+    if (today.getMonth() > 9 || (today.getMonth() === 9 &&  today.getDate() > 2) ){
+      let mynextYearBday = today.getFullYear() + 1;
+       myNextBDay = new Date(mynextYearBday, 09, 02);
+         console.log('myNextBDay', myNextBDay );
+    }
+    */
+      daysBetween = parseInt((myNextBDay.getTime() - today.getTime()) / (24 * 3600 * 1000));
+      daysBetween = daysBetween < 0 ? Math.abs(daysBetween) + ' days ago' : daysBetween + ' days';
     }
 
-    const countDown = document.createElement('div');
-    countDown.setAttribute("id", "birthDays");
-    countDown.innerHTML = '<p> &#127881 &#127873 &#129346 &#127874 : '+ daysUntilMyBDay() + '</p>'
-    document.body.appendChild(countDown);
+
+    return daysBetween;
+  }
+
+  const countDown = document.createElement('div');
+  countDown.setAttribute("id", "birthDays");
+  countDown.innerHTML = '<p> &#127881 &#127873 &#129346 &#127874 : ' + daysUntilMyBDay() + '</p>'
+  document.body.appendChild(countDown);
 
 
-    const pProjects = document.createElement('p');
-    pProjects.innerText = "My Playground Projects";
-    pProjects.setAttribute("class", "category");
-    pProjects.classList.add("projects");
-    document.body.appendChild(pProjects);
+  const pProjects = document.createElement('p');
+  pProjects.innerText = "My Playground Projects";
+  pProjects.setAttribute("class", "category");
+  pProjects.classList.add("projects");
+  document.body.appendChild(pProjects);
 
-    const myProjectList = document.createElement('ul');
-    myProjectList.classList.add("projects");
-    document.body.appendChild(myProjectList);
+  const myProjectList = document.createElement('ul');
+  myProjectList.classList.add("projects");
+  document.body.appendChild(myProjectList);
 
-    const projects = `
+  const projects = `
             
                      <table>
                      
@@ -241,6 +241,32 @@ window.onload = () => {
 
 
                         <tr>
+                          <td> <a href='https://adriana-play-bussines-card.netlify.app/' target="_blank"> Bussines Card </a> </td>
+                          <td> React v6 </td>
+                          <td>
+                            <img src="./img/20_buisinessCard.png" alt="Bussines Card preview picture" width="100" height="150">
+                        </td>
+                         <td>
+                            <p> User: - </p>
+                            <p> Password: - </p>
+                         </td>
+
+
+                        <tr>
+                          <td> <a href='https://adriana-play-tenzies.app/' target="_blank"> Tenzies </a> </td>
+                          <td> React v18 </td>
+                          <td>
+                            <img src="./img/21_tenzies.png" alt="Tenzies preview picture" width="100" height="100">
+                        </td>
+                         <td>
+                            <p> User: - </p>
+                            <p> Password: - </p>
+                         </td>
+
+
+
+
+                        <tr>
                           <td> <a href='https://adriana-play-react-classcomp.netlify.app/' target="_blank"> React - Class Components </a> 
                           </br> &nbsp  vs </br>
                           <a href='https://adriana-play-react-functionalcomp.netlify.app/' target="_blank"> React - Functional Components </a> </td>
@@ -253,6 +279,12 @@ window.onload = () => {
                             <p> Password: - </p>
                          </td>
 
+
+
+
+
+ 
+                   
 
 
  
@@ -300,96 +332,96 @@ window.onload = () => {
                 
                      `;
 
-    myProjectList.innerHTML = projects;                  
+  myProjectList.innerHTML = projects;
 
 
-    const pHobbies = document.createElement('p');
-    pHobbies.setAttribute("class", "category");
-    pHobbies.innerText = 'My Hobbies';
-    document.body.appendChild(pHobbies);
+  const pHobbies = document.createElement('p');
+  pHobbies.setAttribute("class", "category");
+  pHobbies.innerText = 'My Hobbies';
+  document.body.appendChild(pHobbies);
 
-    const hobbies = document.createElement('ul');
-    hobbies.setAttribute('class', 'hobbies');
-    document.body.appendChild(hobbies);
+  const hobbies = document.createElement('ul');
+  hobbies.setAttribute('class', 'hobbies');
+  document.body.appendChild(hobbies);
 
-    let hobbiesArray = ['I like hiking', 'I love nature', 'I like tasty healty food', 'I like to relax doing nothing'];
-    hobbiesArray =  hobbiesArray.map(e => '<li>' + e + '</li>');
-    hobbies.innerHTML = hobbiesArray.join(" ");
-     
-    let liList = document.querySelectorAll('li');
-    liList.forEach(e => e.className = "hobby");
-   
-  
-    const time = document.createElement('li');
-    time.setAttribute("id", "time");
-    time.innerText = 'Currently I live in Bucharest and the current time is: ';
-    hobbies.appendChild(time);
+  let hobbiesArray = ['I like hiking', 'I love nature', 'I like tasty healty food', 'I like to relax doing nothing'];
+  hobbiesArray = hobbiesArray.map(e => '<li>' + e + '</li>');
+  hobbies.innerHTML = hobbiesArray.join(" ");
 
-    function getTime() {
-        const date = new Date;
-        const currentTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds(); 
-        let liTtime = document.getElementById("time");
-        liTtime.innerText = 'Currently I live in Bucharest and the current time is: ' + currentTime;
-      
-    }
-
-    setInterval(getTime, 100);
+  let liList = document.querySelectorAll('li');
+  liList.forEach(e => e.className = "hobby");
 
 
+  const time = document.createElement('li');
+  time.setAttribute("id", "time");
+  time.innerText = 'Currently I live in Bucharest and the current time is: ';
+  hobbies.appendChild(time);
+
+  function getTime() {
+    const date = new Date;
+    const currentTime = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    let liTtime = document.getElementById("time");
+    liTtime.innerText = 'Currently I live in Bucharest and the current time is: ' + currentTime;
+
+  }
+
+  setInterval(getTime, 100);
 
 
 
-   
-const pActivities = document.createElement('p');
-pActivities.setAttribute("class", "category");
-pActivities.innerText = 'My Activities';
 
-document.body.appendChild(pActivities);
 
-const myActivitiesList = document.createElement('ul');
-document.body.appendChild(myActivitiesList);
 
-const activities = `<li>skiing</li>
+  const pActivities = document.createElement('p');
+  pActivities.setAttribute("class", "category");
+  pActivities.innerText = 'My Activities';
+
+  document.body.appendChild(pActivities);
+
+  const myActivitiesList = document.createElement('ul');
+  document.body.appendChild(myActivitiesList);
+
+  const activities = `<li>skiing</li>
                         <li>walking in park</li>
                         <li>scooter rides</li>
                         <li>jogging</li>
                         <li>standup shows</li>
                         <li>travel</li>
                     `
-myActivitiesList.innerHTML = activities;
+  myActivitiesList.innerHTML = activities;
 
-const pRestaurants = document.createElement('p');
-pRestaurants.innerText = "My Favourite Restaurants";
-pRestaurants.setAttribute("class", "category");
-pRestaurants.classList.add("food");
-document.body.appendChild(pRestaurants);
+  const pRestaurants = document.createElement('p');
+  pRestaurants.innerText = "My Favourite Restaurants";
+  pRestaurants.setAttribute("class", "category");
+  pRestaurants.classList.add("food");
+  document.body.appendChild(pRestaurants);
 
-const myRestaurantsList = document.createElement('ul');
-myRestaurantsList.classList.add("restaurants");
-document.body.appendChild(myRestaurantsList);
-const restaurants = `<li>Rawdia</li>
+  const myRestaurantsList = document.createElement('ul');
+  myRestaurantsList.classList.add("restaurants");
+  document.body.appendChild(myRestaurantsList);
+  const restaurants = `<li>Rawdia</li>
                      <li>Cafe Paris</li>
                      <li>Grace</li>
                      `
 
-myRestaurantsList.innerHTML = restaurants; 
+  myRestaurantsList.innerHTML = restaurants;
 
 
-const divOther = document.createElement('div');
-divOther.setAttribute("id", "idOther");
-document.body.appendChild(divOther);
-const h3Other = document.createElement('h3');
-h3Other.innerText = 'And Many More..';
-divOther.appendChild(h3Other);
+  const divOther = document.createElement('div');
+  divOther.setAttribute("id", "idOther");
+  document.body.appendChild(divOther);
+  const h3Other = document.createElement('h3');
+  h3Other.innerText = 'And Many More..';
+  divOther.appendChild(h3Other);
 
-const btn  = document.createElement('button');
-btn.innerText = 'More';
-btn.addEventListener("click", more);
+  const btn = document.createElement('button');
+  btn.innerText = 'More';
+  btn.addEventListener("click", more);
 
-function more() {
+  function more() {
     let elem = document.getElementById("idOther");
     elem.outerHTML = '<p>Love to create web applications that meet the necessities of people</p>';
-}
-divOther.appendChild(btn);
+  }
+  divOther.appendChild(btn);
 }
 
